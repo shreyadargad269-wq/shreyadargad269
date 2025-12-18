288133fe0f0f4957d3aa7a2437c1ff9717632679
shreyadargad269-wq <shreyadargad269@gmail.com>
2025-12-19 00:08:43 +0530

    Add swap program to swap two integers

---
+++ swap.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class swap {
+public static void main(String[] args) {
+	int a=100;
+	int b=200;
+	int temp;
+	temp=b;
+	b=a;
+	a=temp;
+	System.out.println(a);
+	System.out.println(b);
+}
+}
