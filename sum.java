55d3871849f6d19c6cb891299b0a5e7333ff8a02
shreyadargad269-wq <shreyadargad269@gmail.com>
2025-12-19 00:05:29 +0530

    Add sum.java to calculate sum of natural numbers

---
+++ sum.java
@@ -0,0 +1,12 @@
+package corejava;
+
+public class sum {
+public static void main(String[] args) {
+	int n=10;
+	int sum=0;
+	for(int i=1;i<=n;i++) {
+		sum=sum+i;
+	}
+	System.out.println("sum of=" +n+ "natural numbers="+sum);
+}
+}
