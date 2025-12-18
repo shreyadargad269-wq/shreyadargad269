eaea623b267192863d4098b46147870ff1c7b5ac
shreyadargad269-wq <shreyadargad269@gmail.com>
2025-12-19 00:06:44 +0530

    Add sum of digits calculation in sumofdigits.java

---
+++ sumofdigits.java
@@ -0,0 +1,13 @@
+package corejava;
+
+public class sumofdigits {
+public static void main(String[] args) {
+	int num1=100,num2=200;
+	int sum1=num1+num2;
+	int num3=300;
+	int sum2=sum1+num3;
+	int num4=400;
+	int sum3=sum2+num4;
+	System.out.println("sum="+sum3);
+}
+}
