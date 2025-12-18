70e12abd26c2d76ebd8cf59e18f400d052ae319f
shreyadargad269-wq <shreyadargad269@gmail.com>
2025-12-18 23:55:04 +0530

    Create narrowing.java

---
+++ narrowing.java
@@ -0,0 +1,15 @@
+package corejava;
+
+public class narrowing {
+public static void main(String[] args) {
+	int x=10000;
+	int y=20000;
+	long ans=(long)x*y;
+	System.out.println(ans);
+	int a=15;
+	int b=10;
+	float res=(float)a/b;
+	System.out.println(res);
+	
+}
+}
