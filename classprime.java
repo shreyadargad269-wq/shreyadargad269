c8a8deb17078843027a69a90b75db2bc33a2c3e6
shreyadargad269-wq <shreyadargad269@gmail.com>
2025-12-18 23:59:13 +0530

    Create classprime.java

---
+++ classprime.java
@@ -0,0 +1,19 @@
+package corejava;
+
+public class prime {
+public static void main(String[] args) {
+	int num=8;
+	int count=0;
+	for(int i=1;i<=num;i++) {
+		if(num%i==0) {
+			count++;
+		}
+	}
+	if(count==2) {
+		System.out.println(num+" is prime");
+	}
+	else {
+		System.out.println(num+" is not prime");
+	}
+}
+}
