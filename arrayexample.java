9b3073e2978df132122ec0a40572283f146a4e97
shreyadargad269-wq <shreyadargad269@gmail.com>
2025-12-18 23:32:18 +0530

    Add array example that initializes and prints values

---
+++ arrayexample.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class arrayexample {
+	public static void main(String[] args) {
+		int arr[];
+		arr = new int[6];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i * 3;
+		}
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
