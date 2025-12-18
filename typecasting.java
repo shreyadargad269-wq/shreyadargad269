92e6274c83c70245439d00907b4ede82b0f01842
shreyadargad269-wq <shreyadargad269@gmail.com>
2025-12-19 00:14:02 +0530

    Create typecasting.java

---
+++ typecasting.java
@@ -0,0 +1,12 @@
+package corejava;
+
+public class typecasting {
+public static void main(String[] args) {
+	int i=10;
+	long l=i;
+	double d=i;
+	System.out.println("int i="+i);
+	System.out.println("long l="+l);
+	System.out.println("double d="+d);
+}
+}
