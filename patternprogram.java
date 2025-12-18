44eaf3b4ca81f7e78fc3549013d2b6bbed0336ac
shreyadargad269-wq <shreyadargad269@gmail.com>
2025-12-18 23:55:56 +0530

    Add pattern printing program in Java

---
+++ patternprogram.java
@@ -0,0 +1,13 @@
+package corejava;
+
+public class patternProgram {
+public static void main(String[] args) {
+	int row=5;
+	for(int i=row;i>=1;i--) {
+		for(int j=1;j<=i;j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+}
+}
