0f923b0d23e7eb0077c091cc178c77553f5ad9b2
shreyadargad269-wq <shreyadargad269@gmail.com>
2025-12-19 00:09:11 +0530

    Implement swap algorithm in swap1.java

---
+++ swap1.java
@@ -0,0 +1,13 @@
+package corejava;
+
+public class swap1 {
+public static void main(String[] args) {
+	int a=100;
+	int b=200;
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	System.out.println("a="+a);
+	System.out.println("b="+b);
+}
+}
