29be547caedc51e45cc1296c0e0fc72ab608874e
shreyadargad269-wq <shreyadargad269@gmail.com>
2025-12-18 23:34:35 +0530

    Add boolean program to check boolean value

---
+++ booleanprogram.java
@@ -0,0 +1,11 @@
+package corejava;
+
+public class booleanprogram {
+public static void main(String[] args) {
+	boolean a=true;
+	if(a==false)
+	System.out.println(true);
+	else 
+		System.out.println(false);
+}
+}
